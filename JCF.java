ab0437ca8ce62685ef025b7f2d0ec1e23d13a3c3
SaiShruthiReddyD <saishruthidhesham@gmail.com>
2026-03-17 20:42:48 +0530

    Add files via upload

---
+++ JCF.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class JCF {
+    public static void main(String args[]){
+        ArrayList<Integer> list=new ArrayList<>();
+        List<Integer> list1=new ArrayList<>();
+        Set<Integer> set=new HashSet<>();
+        HashMap<Integer,Integer> map=new HashMap<>();
+    }
+}

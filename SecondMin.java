bdc90e8bb26a11f77c780201bae24d794aa620ea
SaiShruthiReddyD <saishruthidhesham@gmail.com>
2026-04-03 18:58:27 +0530

    Add files via upload

---
+++ SecondMin.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class SecondMin {
+    public static void main(String args[]){
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        int[] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=sc.nextInt();
+        }
+        int min1;
+        int min2;
+        if(arr[0]<arr[1]){
+            min1=arr[0];
+            min2=arr[1];
+        }
+        else{
+            min1=arr[1];
+            min2=arr[0];
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]<min1){
+                min2=min1;
+                min1=arr[i];
+            }
+        }System.out.println(min2);
+    }
+}

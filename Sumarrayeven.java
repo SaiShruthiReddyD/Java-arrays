613f7deffa6cb2abf35da6bdbad3fdf54cf799ea
SaiShruthiReddyD <saishruthidhesham@gmail.com>
2026-04-06 19:34:45 +0530

    Add files via upload

---
+++ Sumarrayeven.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Sumarrayeven{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int sum=0;
+		int[] arr=new int[n];
+		for(int i=0;i<n;i++){
+		    arr[i]=sc.nextInt();
+		}
+		for(int i=0;i<n;i++){
+		    if(arr[i]%2==0){
+		    sum=sum+arr[i];
+		    }
+		}
+		System.out.println(sum);
+	}
+}
